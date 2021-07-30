e45fafa18bccc1b77782f35c026a9c0a54cc3e49
Ferakp <akpinarferidun92@icloud.com>
2021-07-30 21:46:31 +0300

    Readjust row sign (force to take full height) and add message with blue color

--- src/components/console.js
+++ src/components/console.js
@@ -11,7 +11,8 @@ export class Console {
   messages = [];
 
   attached(){
-    this.messages.push({text: "This is a test!", color: "red"});
-    this.messages.push({text: "This is a test!", color: "green"});
+    this.messages.push({text: "This is a test.", color: "red"});
+    this.messages.push({text: "This message should be distinguished with green sign.", color: "green"});
+    this.messages.push({text: "This is a message with blue color.", color: "blue"});
   }
 }
