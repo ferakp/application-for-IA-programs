c0331590934dd851e19e6d5df12b22f07ffbf73f
Ferakp <akpinarferidun92@icloud.com>
2021-10-17 19:27:28 +0300

    Change the name of the view to agents-view

---
+++ src/components/agents-view.js
@@ -0,0 +1,4 @@
+
+export class AgentsView {
+  
+}
--- src/resources/index.js
+++ src/resources/index.js
@@ -5,7 +5,7 @@ export function configure(config) {
     [
       PLATFORM.moduleName('../components/text-field'),
       PLATFORM.moduleName('../components/checkbox'),
-      PLATFORM.moduleName('../components/agent-view'),
+      PLATFORM.moduleName('../components/agents-view'),
       PLATFORM.moduleName('../components/landing'),
       PLATFORM.moduleName('../components/combobox'),
       PLATFORM.moduleName('../components/combobox-tag'),
