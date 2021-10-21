cafa146b529de28f7eee80b4342a6de67742379e
Ferakp <akpinarferidun92@icloud.com>
2021-10-22 02:56:20 +0300

    Add functionality for deleting file

--- src/components/files-view.js
+++ src/components/files-view.js
@@ -1,11 +1,16 @@
 import { bindable } from "aurelia-framework";
 
 export class FilesView {
-  
   @bindable
   label;
 
   @bindable
   files;
 
+  deleteFile = (file) => {
+    for (let i = 0; i < this.files.length; i++) {
+      if (this.files[i].name === file.name && this.files[i].size === file.size)
+        this.files.splice(i, 1);
+    }
+  };
 }
