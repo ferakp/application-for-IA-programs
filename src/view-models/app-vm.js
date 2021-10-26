fa733c23e47fafb017ea30a81cbc6953e6b73f47
Ferakp <akpinarferidun92@icloud.com>
2021-10-26 03:36:45 +0300

    Add parser for rules

--- src/view-models/app-vm.js
+++ src/view-models/app-vm.js
@@ -56,13 +56,13 @@ export class AppVM {
     this.logs.push(new Log('A new agent has been created \n with producer ID (Agent ID) of ' + agent.id, null, null, null, { id: agent.id }));
   };
 
-  log = (message, producerId) => {
-    this.logs.push(new Log(message, null, null, null, { id: producerId }));
+  log = (message, producerId, status) => {
+    this.logs.push(new Log(message, null, null, null, { id: producerId }, status));
   };
 
   createTerminalLine = (text, isResponse) => {
-    this.terminalLines.push({text: text, isResponse: isResponse})
-  }
+    this.terminalLines.push({ text: text, isResponse: isResponse });
+  };
 }
 
 class Agent {
@@ -77,15 +77,17 @@ class Agent {
   type;
 
   // Only if agent's type is 0 and 1
-  ruleActionMap;
+  ruleActionList = [];
 
   // perceptions
   perceptions;
 
+  goalFunction;
+
   // Only if agent's type is 3
   utilityFunction;
 
-  // Status - initializing, initialized, running, ready
+  // Status - initializing, initialized, running, ready, error
   state = 'initializing';
 
   startTime = 'N/A';
@@ -115,21 +117,172 @@ class Agent {
       this.appVMApi.log('Initializing ' + this.name, this.id);
       let { response, errorMessage } = await this.appVMApi.fileReader.readFile(this.file);
       if (Array.isArray(response)) {
-        this.perceptions = [];
-        response.forEach(e => this.perceptions.push(e));
-        this.appVMApi.log(this.name + ' has been initialized. ' + this.perceptions.length + ' perceptions have been extracted from the file ' + this.file.name, this.id);
-        this.state = 'initialized';
+        let answer = this.parseRules(response.filter(e => e).map(e => e.trim()));
+        if (answer) this.state = 'initialized';
+        else this.state = 'error';
       }
-    }
-    if (state === 'run') {
+    } else if (state === 'run') {
       this.appVMApi.log('Running ' + this.name, this.id);
-
     }
   }
+
+  parseRules = rules => {
+    if (!this.isRulesValid(rules)) {
+      this.appVMApi.log('Unable to parse rules from the file. Invalid syntax.', null);
+      return false;
+    }
+
+    let response = true;
+    rules.forEach(e => {
+      let splittedRule = e.split(' ');
+      let formattedRule = [];
+
+      // formattedValues
+      let id = this.parseIdValue(splittedRule[1]);
+      let target = this.parseTargetValue(splittedRule[3]);
+      let rule = this.parseRuleValue(splittedRule[5]);
+      let action = this.parseActionValue(splittedRule[7]);
+
+      console.log(id, target, rule, action);
+
+      if ([id, target, rule, action].includes(false)) {
+        this.reportInvalidAttributeValue(e);
+        this.response = false;
+      } else {
+        formattedRule.push(...[id, target, rule, action]);
+      }
+      this.ruleActionList.push(formattedRule);
+    });
+    return response;
+  };
+
+  reportInvalidAttributeValue(e) {
+    this.appVMApi.log('Unable to register a rule. Invalid attribute value(s). Invalid rule : ' + e, null, [false, 'Invalid attribute values']);
+    this.appVMApi.log('Check documentation for syntax of ' + ['Reflex', 'Model-based reflex', 'Goal-based', 'Utility-based'][this.type] + ' agent rules', null, [false, 'Invalid attribute values']);
+  }
+
+  executeAction = action => {};
 
   delete() {
     this.appVMApi.deleteAgent(this.id);
   }
+
+  /**
+   * UTILITY FUNCTIONS
+   */
+
+  /**
+   *
+   * @param {string} value
+   * @returns {Array} action or [əction, action]
+   */
+  parseActionValue = value => {
+    if ((this.type === 0 || this.type === 1) && value.length > 0) {
+      return value;
+    } else if ((this.type === 2 || this.type === 3) && value.split(':').length === 2) {
+      let answer = value.split(':');
+      if (answer[0] && answer[1]) return answer;
+      else return false;
+    } else return false;
+  };
+
+  /**
+   *
+   * @param {string} value
+   * @returns {Array} [operator, value, (secondValue)] or false
+   */
+  parseRuleValue = value => {
+    if ((this.type === 0 || this.type === 2 || this.type === 3) && this.containsElement(value, ['<', '>', '='])) {
+      let answer;
+      if (value.includes('<')) answer = ['<', parseFloat(value.replace('<', ''))];
+      else if (value.includes('>')) answer = ['>', parseFloat(value.replace('>', ''))];
+      else if (value.includes('=')) answer = ['=', parseFloat(value.replace('=', ''))];
+      else return false;
+      if (!isNaN(answer[1])) return answer;
+      else return false;
+    } else if (this.type === 1 && this.containsElement(value, ['<', '>', '=']) && this.containsElement(value, [':']) && value.split(':').length === 2) {
+      let answer;
+      if (value.includes('<')) answer = ['<', parseFloat(value.split(':')[0].replace('<', '')), parseFloat(value.split(':')[1])];
+      else if (value.includes('>')) answer = ['>', parseFloat(value.split(':')[0].replace('>', '')), parseFloat(value.split(':')[1])];
+      else if (value.includes('=')) answer = ['=', parseFloat(value.split(':')[0].replace('=', '')), parseFloat(value.split(':')[1])];
+      else return false;
+      if (isNaN(answer[1]) || isNaN(answer[2])) return false;
+      else return answer;
+    } else return false;
+  };
+
+  /**
+   *
+   * @param {string} value
+   * @returns {Object} value or false
+   */
+  parseTargetValue = value => {
+    if (value === '*') {
+      return '*';
+    } else if (value.length > 0) {
+      return value;
+    } else return false;
+  };
+
+  /**
+   *
+   * @param {string} idValue
+   * @returns {Object} value or false
+   */
+  parseIdValue = idValue => {
+    if (idValue === '*') {
+      return '*';
+    } else if (typeof parseFloat(idValue) === 'number') {
+      return parseFloat(idValue);
+    } else return false;
+  };
+
+  isRulesValid = rules => {
+    let answer = null;
+    let ruleType = this.type;
+    // Example ID 001 TARGET TEMPERATURE RULE <22:5 ACTION NOTIFICATION:RED
+    if (ruleType === 0) {
+      rules.forEach(e => {
+        console.log(e);
+        let ruleSplitted = e.split(' ');
+        if (ruleSplitted.length !== 8) answer = false;
+        if (!(ruleSplitted[0] === 'ID' && ruleSplitted[2] === 'TARGET' && ruleSplitted[4] === 'RULE' && ruleSplitted[6] === 'ACTION')) answer = false;
+        if (!(ruleSplitted[5].includes('<') || ruleSplitted[5].includes('>') || ruleSplitted[5].includes('='))) answer = false;
+      });
+    } else if (ruleType === 1) {
+      rules.forEach(e => {
+        let ruleSplitted = e.split(' ');
+        if (ruleSplitted.length !== 8) answer = false;
+        if (!(ruleSplitted[0] === 'ID' && ruleSplitted[2] === 'TARGET' && ruleSplitted[4] === 'RULE' && ruleSplitted[6] === 'ACTION')) answer = false;
+        if (!(ruleSplitted[5].includes('<') || ruleSplitted[5].includes('>') || ruleSplitted[5].includes('=') || ruleSplitted[5].includes(':'))) answer = false;
+      });
+    } else if (ruleType === 2) {
+      rules.forEach(e => {
+        let ruleSplitted = e.split(' ');
+        if (ruleSplitted.length !== 8) answer = false;
+        if (!(ruleSplitted[0] === 'ID' && ruleSplitted[2] === 'TARGET' && ruleSplitted[4] === 'RULE' && ruleSplitted[6] === 'ACTION')) answer = false;
+        if (!(ruleSplitted[5].includes('<') || ruleSplitted[5].includes('>') || ruleSplitted[5].includes('='))) answer = false;
+      });
+    } else if (ruleType === 3) {
+      rules.forEach(e => {
+        let ruleSplitted = e.split(' ');
+        if (ruleSplitted.length !== 8) answer = false;
+        if (!(ruleSplitted[0] === 'ID' && ruleSplitted[2] === 'TARGET' && ruleSplitted[4] === 'RULE' && ruleSplitted[6] === 'ACTION')) answer = false;
+        if (!(ruleSplitted[5].includes('<') || ruleSplitted[5].includes('>') || ruleSplitted[5].includes('='))) answer = false;
+      });
+    } else answer = false;
+
+    if (answer === false) return false;
+    if (answer === null) return true;
+  };
+
+  containsElement = (value, arr) => {
+    let answer = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (value.includes(arr[i])) answer = true;
+    }
+    return answer;
+  };
 }
 
 class Log {
@@ -137,10 +290,11 @@ class Log {
   id;
   color;
   time;
+  status;
 
   producer;
 
-  constructor(text, id, color, time, producer) {
+  constructor(text, id, color, time, producer, status) {
     if (text) this.text = text;
     else this.text = '';
     if (id) this.id = id;
@@ -151,6 +305,8 @@ class Log {
     else this.time = new Date();
     if (producer) this.producer = producer;
     else this.producer = { id: 'N/A' };
+    if (status) this.status = status;
+    else this.status = [];
   }
 
   generateNewId() {
