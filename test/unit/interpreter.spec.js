69bcd697ca8a10ae0b451811b21fcdc48d9693d5
Feridun <akpinarferidun92@icloud.com>
2021-10-31 14:58:41 +0000

    Update interpreter unit test

--- test/unit/interpreter.spec.js
+++ test/unit/interpreter.spec.js
@@ -1,17 +1,14 @@
 import { update, executeTest, expectElement, expectElementAttribute, expectElementNumber, expectViewModelProperty } from '../test-utils';
-import { Container } from 'aurelia-dependency-injection';
 import { Interpreter } from '../../src/interpreter/interpreter';
 
 describe('Test Interpreter class', () => {
-  const container = new Container();
-  const instance = container.get(Interpreter);
+  let interpreter;
 
   beforeEach(() => {
-    container = new Container();
-    instance = container.get(Interpreter);
+    interpreter = new Interpreter();
   });
 
   it('has correct agent types', () => {
-    expect(instance.agentTypes).toBe(4);
+    expect(interpreter.agentTypes).toBe(4);
   });
 });
