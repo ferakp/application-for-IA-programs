dc1662922d767bd07eb54ef455577412f4306e83
ferakp <akpinarferidun92@icloud.com>
2021-11-05 11:04:13 +0000

    Add e2e tests for upload component

--- cypress/integration/upload.e2e.js
+++ cypress/integration/upload.e2e.js
@@ -1,12 +1,13 @@
+import 'cypress-file-upload';
+
 describe('E2E test for upload component', () => {
   beforeEach(() => {
-    cy.visit('/dashboard');
+    cy.visit('https://simple-ia-app.ferakp.ovh/dashboard');
     cy.get(".dashboard-menu__files-view-element").click();
   });
 
   it('should activate upload component', () => {
-    cy.get('input[type="file]"').attachFile("files/sample.txt");
-    cy.get("#file-sumbit").click();
+    cy.get('input[type="file"]').attachFile("sample.txt", {allowEmpty: true});
     cy.get(".files-view__file-name-tooltip-text").contains("sample.txt");
   });
 });
