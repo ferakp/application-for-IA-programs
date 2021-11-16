6e63f09fe56ac92e306549d8407dff9e639f88a0
ferakp <akpinarferidun92@icloud.com>
2021-11-16 21:52:13 +0000

    Add e2e tests for creating, running and verifying agents

--- cypress/integration/dashboard.e2e.js
+++ cypress/integration/dashboard.e2e.js
@@ -3,65 +3,186 @@ import 'cypress-file-upload';
 describe('E2E tests for application functionalities', () => {
 
   // Creates a file representation for cypres-file-upload library
-  const createFileReps = (fileContent, fileName) => {
-    return {
+  const createFileAndUpload = (fileContent, fileName) => {
+    cy.get('input[type="file"]').attachFile({
       fileContent: fileContent.toString(),
       fileName: fileName,
       encoding: 'utf8',
       mimeType: 'text/plain',
       lastModified: new Date().getTime()
-    };
+    });
+  }
+
+  const createAgent = (type, fileIndex) => {
+    writeToTerminal("create agent -file " + fileIndex + " -class " + type);
+  }
+
+  const writeToTerminal = (text) => {
+    cy.get(".dashboard__console-container .text-field__input-element").type(text);
+    cy.get(".dashboard__console-container .text-field__input-element").type("{enter}");
+  }
+
+  const hasPrintedLogWithText = (text) => {
+    cy.get(".logs-view__console").contains(text);
+  }
+
+  const runAgent = () => {
+    cy.get(".agent-box__content-row:nth-of-type(2) span:nth-of-type(2)").then(el => {
+      writeToTerminal("run agent " + el.text());
+    });
   }
 
   beforeEach(() => {
     cy.visit('https://simple-ia-app.ferakp.ovh/dashboard');
   });
 
+  /**
+   * E2E TESTS FOR TESTING FILE UPLOAD AND AGENT CREATION FEATURE
+   */
+
   it('creates reflex agent', () => {
-    let fileReps = createFileReps("ID 001 TARGET TEMPERATURE RULE <22 ACTION GO_UP", "sample.txt");
-    cy.get("agent-box").should('not.exist');
-    cy.get('input[type="file"]').attachFile(fileReps);
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22 ACTION GO_UP", "sample.txt");
+    createAgent("reflex", 0);
     cy.get(".files-view__file-name").contains("sample.txt");
-    cy.get(".dashboard__console-container .text-field__input-element").type("create agent -file 0 -class reflex");
-    cy.get(".dashboard__console-container .text-field__input-element").type("{enter}");
     cy.wait(3000);
     cy.get("agent-box").should('exist');
     cy.get("agent-box .text-field__input-element").should("have.value", "initialized");
   });
 
   it('creates model-based reflex agent', () => {
-    let fileReps = createFileReps("ID 001 TARGET TEMPERATURE RULE <22:1000 ACTION GO_UP", "sample.txt");
-    cy.get("agent-box").should('not.exist');
-    cy.get('input[type="file"]').attachFile(fileReps);
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22:1000 ACTION GO_UP", "sample.txt");
+    createAgent("model-reflex", 0);
     cy.get(".files-view__file-name").contains("sample.txt");
-    cy.get(".dashboard__console-container .text-field__input-element").type("create agent -file 0 -class model-reflex");
-    cy.get(".dashboard__console-container .text-field__input-element").type("{enter}");
     cy.wait(3000);
     cy.get("agent-box").should('exist');
     cy.get("agent-box .text-field__input-element").should("have.value", "initialized");
   });
 
   it('creates goal-based agent', () => {
-    let fileReps = createFileReps("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
-    cy.get("agent-box").should('not.exist');
-    cy.get('input[type="file"]').attachFile(fileReps);
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("goal", 0);
     cy.get(".files-view__file-name").contains("sample.txt");
-    cy.get(".dashboard__console-container .text-field__input-element").type("create agent -file 0 -class goal");
-    cy.get(".dashboard__console-container .text-field__input-element").type("{enter}");
     cy.wait(3000);
     cy.get("agent-box").should('exist');
     cy.get("agent-box .text-field__input-element").should("have.value", "initialized");
   });
 
   it('creates utility-based agent', () => {
-    let fileReps = createFileReps("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
-    cy.get("agent-box").should('not.exist');
-    cy.get('input[type="file"]').attachFile(fileReps);
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("utility", 0);
     cy.get(".files-view__file-name").contains("sample.txt");
-    cy.get(".dashboard__console-container .text-field__input-element").type("create agent -file 0 -class goal");
-    cy.get(".dashboard__console-container .text-field__input-element").type("{enter}");
     cy.wait(3000);
     cy.get("agent-box").should('exist');
     cy.get("agent-box .text-field__input-element").should("have.value", "initialized");
   });
+
+  /**
+   * E2E TESTS FOR RUNNING AGENTS
+   */
+  it('runs reflex agent', () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22 ACTION GO_UP", "sample.txt");
+    createAgent("reflex", 0);
+    cy.wait(3000);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    cy.get("agent-box .text-field__input-element").should("have.value", "running");
+
+  });
+
+  it('runs model-based reflex agent', () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22:1000 ACTION GO_UP", "sample.txt");
+    createAgent("model-reflex", 0);
+    cy.wait(3000);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    cy.get("agent-box .text-field__input-element").should("have.value", "running");
+
+  });
+
+  it('runs goal-based agent', () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("goal", 0);
+    cy.wait(3000);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    cy.get("agent-box .text-field__input-element").should("have.value", "running");
+
+  });
+
+  it('runs utility-based agent', () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("utility", 0);
+    cy.wait(3000);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    cy.get("agent-box .text-field__input-element").should("have.value", "running");
+
+  });
+
+  /**
+   * E2E TESTS FOR TESTING AGENT OUTPUTS
+   */
+
+  it("chooses correct action (reflex agent)", () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22 ACTION GO_UP", "sample.txt");
+    createAgent("reflex", 0);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    writeToTerminal("generate perception id 1 target temperature value 18");
+    cy.wait(4000);
+    hasPrintedLogWithText("GO_UP HAS BEEN ACTIVATED");
+  });
+
+  it("chooses correct action (model-based reflex agent)", () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE <22:1000 ACTION GO_UP", "sample.txt");
+    createAgent("model-reflex", 0);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    writeToTerminal("generate perception id 1 target temperature value 18");
+    cy.wait(1000);
+    writeToTerminal("generate perception id 1 target temperature value 18");
+    cy.wait(3000);
+    hasPrintedLogWithText("GO_UP HAS BEEN ACTIVATED");
+  });
+
+  it("chooses correct action (goal-based agent)", () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("goal", 0);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(2000);
+    writeToTerminal("generate perception id 1 target temperature value 18");
+    cy.wait(3000);
+    hasPrintedLogWithText("GO_UP HAS BEEN ACTIVATED");
+    writeToTerminal("generate perception id 1 target temperature value 28");
+    cy.wait(3000);
+    hasPrintedLogWithText("GO_DOWN HAS BEEN ACTIVATED");
+  });
+
+  it("chooses correct action (utility-based agent)", () => {
+    createFileAndUpload("ID 001 TARGET TEMPERATURE RULE 22 ACTION GO_UP:GO_DOWN", "sample.txt");
+    createAgent("utility", 0);
+    cy.wait(1500);
+    runAgent();
+    cy.wait(1500);
+    writeToTerminal("generate perception id 1 target temperature value 18");
+    writeToTerminal("generate perception id 1 target temperature value 16");
+    writeToTerminal("generate perception id 1 target temperature value 17");
+    cy.wait(3000);
+    hasPrintedLogWithText("GO_UP HAS BEEN ACTIVATED");
+    writeToTerminal("generate perception id 1 target temperature value 28");
+    writeToTerminal("generate perception id 1 target temperature value 26");
+    cy.wait(3000);
+    hasPrintedLogWithText("GO_DOWN HAS BEEN ACTIVATED");
+    writeToTerminal("generate perception id 1 target temperature value 22");
+    cy.wait(3000);
+    hasPrintedLogWithText("HAS REACHED ITS GOAL");
+  });
+
 });
