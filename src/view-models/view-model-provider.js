62fb11978254dfb9836d5e97604fa17801724500
ferakp <akpinarferidun92@icloud.com>
2021-11-17 21:07:57 +0000

    Add comments

--- src/view-models/view-model-provider.js
+++ src/view-models/view-model-provider.js
@@ -1,8 +1,16 @@
 import { AppVM } from './app-vm';
 
+/**
+ * Class for providing AppVM instance
+ */
 export class ViewModelProvider {
   appVM;
 
+  /**
+   * Returns appVM if it exists otherwise creates a new one and return
+   * recently created instance
+   * @returns {AppVM}
+   */
   getAppVM = () => {
     if (!this.appVM) this.appVM = new AppVM();
     return this.appVM;
